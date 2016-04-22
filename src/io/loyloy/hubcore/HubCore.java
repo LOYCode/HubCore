5bb97624a6f169d3b7cb813fed18425def6ee6fa
Daniel Murray <daniel@nonit.me>
2016-04-22 03:53:30 +0100

    Added code

---
+++ src/io/loyloy/hubcore/HubCore.java
@@ -0,0 +1,17 @@
+package io.loyloy.hubcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HubCore extends JavaPlugin
+{
+    public HubCore()
+    {
+
+    }
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
